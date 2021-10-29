37f61e43f9bb628441f09b79cfbfd94d148c342f
Antonio Ribeiro <antonioribdev@gmail.com>
2021-10-29 14:27:54 -0300

    main layout

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+
+export default function Footer() {
+    return (
+        <Flex height='65px' w='100%'>
+            Footer
+        </Flex>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Flex
+      height="65px"
+      w="100%"
+      bgColor="white"
+      shadow="md"
+    >
+      Header
+    </Flex>
+  );
+}
